c9c168e5948325ce4abfdd0cd77ae57634d3abf5
theharishjangid <harish.n.jangid@gmail.com>
2020-01-27 11:34:08 +0530

    changed the mapping

--- classroom/serializers.py
+++ classroom/serializers.py
@@ -1,12 +1,17 @@
-from .models import ClassRoom, ClassRoomStaffMapping
-from rest_framework.serializers import ModelSerializer
+from .models import ClassRoom, ClassRoomStaffMapping, StudentClassRoomMapping
+from rest_framework import serializers
 
-class ClassRoomSerializer(ModelSerializer):
+class ClassRoomSerializer(serializers.ModelSerializer):
     class Meta:
         model = ClassRoom
         fields = '__all__'
 
-class ClassRoomStaffMappingSerializer(ModelSerializer):
+class ClassRoomStaffMappingSerializer(serializers.ModelSerializer):
     class Meta:
         model = ClassRoomStaffMapping
+        fields = '__all__'
+
+class StudentClassRoomMappingSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = StudentClassRoomMapping
         fields = '__all__'
--- classroom/urls.py
+++ classroom/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
-from .views import ClassRoomList, ClassRoomDetail, ClassRoomStaffMappingList, ClassRoomStaffMappingDetail
+from .views import (ClassRoomList, ClassRoomDetail, ClassRoomStaffMappingList, ClassRoomStaffMappingDetail,
+                    StudentClassRoomMappingList, StudentClassRoomMappingDetail)
 
 urlpatterns = [
     path('classroom/', ClassRoomList.as_view()),
     path('classroom/<int:pk>/', ClassRoomDetail.as_view()),
     path('classroomstaff/', ClassRoomStaffMappingList.as_view()),
     path('classroomstaff/<int:pk>/', ClassRoomStaffMappingDetail.as_view()),
+    path('studentclass/', StudentClassRoomMappingList.as_view()),
+    path('studentclass/<int:pk>/', StudentClassRoomMappingDetail.as_view()),
 ]
